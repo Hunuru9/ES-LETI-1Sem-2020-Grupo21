16c90f99c6ddbc12a56dda06399eb444e38f2f59
Francisco <fsnsa@iscte-iul.pt>
2020-10-30 19:49:18 +0000

    Excel info in GUI v0.2

--- Code-Smell.service/src/main/java/CodeSmell/Project/Gui.java
+++ Code-Smell.service/src/main/java/CodeSmell/Project/Gui.java
@@ -3,6 +3,8 @@ package CodeSmell.Project;
 import java.awt.*;
 import java.io.*;
 import javax.swing.*;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.swing.JFrame;
@@ -18,6 +20,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class Gui {
 	private JFrame frame;
 	private JPanel excel = new JPanel();
+	String[][] data = new String[numofRowsExcel()][numofColumnsExcel()];
+	String[] columnNames = {"First Name", "Last Name","Sport","Vegetarian"};
 
 	public Gui() throws IOException, ClassNotFoundException {
 
@@ -25,7 +29,8 @@ public class Gui {
 		frame=new JFrame("Code Smells");
 		frame.setLayout(new BorderLayout());
 		excel.setPreferredSize(new Dimension(800,800));
-		excel.setLayout(new GridLayout());
+		excel.setLayout(new FlowLayout());
+		JTable table = new JTable(data, columnNames);
 		JScrollPane scroll2=new JScrollPane(excel);
 		frame.add(scroll2, BorderLayout.CENTER);
 
@@ -51,7 +56,7 @@ public class Gui {
 						null);
 			}
 		});
-		
+
 		showExcel.addActionListener(new java.awt.event.ActionListener() {
 			@Override
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -70,6 +75,29 @@ public class Gui {
 
 	}
 
+	public int numofRowsExcel() throws IOException {
+		String excelFilePath = "Defeitos.xlsx";
+		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		Sheet firstSheet = workbook.getSheetAt(0);
+		Iterator<Row> iterator = firstSheet.iterator();
+		return firstSheet.getLastRowNum();
+	}
+
+	public int numofColumnsExcel() throws IOException {
+		String excelFilePath = "Defeitos.xlsx";
+		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		Sheet firstSheet = workbook.getSheetAt(0);
+		Iterator<Row> iterator = firstSheet.iterator();
+
+		Row nextRow = iterator.next();
+		Iterator<Cell> cellIterator = nextRow.cellIterator();
+		return nextRow.getLastCellNum();
+	}
+
 	public void readExcelFile() throws IOException {
 		String excelFilePath = "Defeitos.xlsx";
 		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
@@ -85,24 +113,25 @@ public class Gui {
 			while (cellIterator.hasNext()) {
 				Cell cell = cellIterator.next();
 
+
 				switch (cell.getCellType()) {
 				case Cell.CELL_TYPE_STRING:
-					JLabel cellStringContent = new JLabel(cell.getStringCellValue());
-					excel.add(cellStringContent);
-					//System.out.println(cell.getStringCellValue());
+					//					JLabel cellStringContent = new JLabel(cell.getStringCellValue());
+					data[cell.getRowIndex()][cell.getColumnIndex()]=cell.getStringCellValue();
+					//					System.out.println(cell.getStringCellValue());
 					break;
 				case Cell.CELL_TYPE_BOOLEAN:
-					JLabel cellBooleanContent = new JLabel(Boolean.toString(cell.getBooleanCellValue()));
-					excel.add(cellBooleanContent);
-					//System.out.print(cell.getBooleanCellValue());
+					//					JLabel cellBooleanContent = new JLabel(Boolean.toString(cell.getBooleanCellValue()));
+					data[cell.getRowIndex()][cell.getColumnIndex()]=Boolean.toString(cell.getBooleanCellValue());
+					//					System.out.print(cell.getBooleanCellValue());
 					break;
 				case Cell.CELL_TYPE_NUMERIC:
-					JLabel cellIntContent = new JLabel(Double.toString(cell.getNumericCellValue()));
-					excel.add(cellIntContent);
-					//System.out.print(cell.getNumericCellValue());
+					//					JLabel cellIntContent = new JLabel(Double.toString(cell.getNumericCellValue()));
+					data[cell.getRowIndex()][cell.getColumnIndex()]=Double.toString(cell.getNumericCellValue());
+					//					System.out.print(cell.getNumericCellValue());
 					break;
 				}
-				//System.out.print(" - ");
+				System.out.print(" - ");
 			}
 			System.out.println();
 		}
