99eadbb3bec2f2d91efe0a9f857d8a33df556079
Hunuru9 <hacls@iscte-iul.pt>
2020-10-30 16:58:18 +0000

    Importação do ficheiro excel para ler o seu conteúdo na consola

---
+++ Code-Smell.service/src/main/java/CodeSmell/Project/App.java
@@ -0,0 +1,25 @@
+package CodeSmell.Project;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+		System.out.println("Hi there!");
+    }
+    
+    
+}
