4c8cd6e9a555c615df289f990b61aca319aa2aa8
Hunuru9 <hacls@iscte-iul.pt>
2020-12-07 19:36:28 +0000

    Method quality_indicators and some updates in another methods

--- Code-Smell.service/src/main/java/codesmellservice/App.java
+++ Code-Smell.service/src/main/java/codesmellservice/App.java
@@ -1,6 +1,7 @@
 package codesmellservice;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Hello world!
@@ -14,6 +15,18 @@ public class App
 		//excelReader.getMetricValues("LOC");
 		Gui gui = new Gui(excelReader, ruleSet);
 		gui.open();
+		
+		Rule r=new Rule("is_long_method");
+        r.setNomeRegra("BOAS");
+        r.setMetricaX(80.0);
+        r.setmetricaXOperator(">");
+        r.setMetricaY(10.0);
+        r.setmetricaYOperator(">");
+        r.setLogicalOperator("AND");
+        List <String> firstColumn=excelReader.getColumnValues("LOC");
+        List <String> secondColumn=excelReader.getColumnValues("CYCLO");
+        String aux=r.getmetricaXOperator()+r.getmetricaYOperator();
+        ruleSet.quality_indicators(r, ruleSet.stringValues(aux, r, firstColumn, secondColumn));
     }
 	
 	
