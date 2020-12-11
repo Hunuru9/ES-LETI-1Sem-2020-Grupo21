0a07ca962f6545e03a11fc2170162b1d8000a6b2
gfmoo1 <gfmoo1@iscte-iul.pt>
2020-12-11 00:43:07 +0000

    Documentação javadoc da classe RuleSet

--- Code-Smell.service/src/main/java/codesmellservice/App.java
+++ Code-Smell.service/src/main/java/codesmellservice/App.java
@@ -11,25 +11,6 @@ public class App
 		RuleSet lista=new RuleSet(excelReader);
 		Gui gui = new Gui(excelReader, lista);
 		gui.open();
-		/*Rule r=new Rule("is_long_method");
-		r.setNomeRegra("BOAS");
-		r.setMetricaX(80.0);
-		r.setmetricaXOperator(">");
-		r.setMetricaY(10.0);
-		r.setmetricaYOperator(">");
-		r.setLogicalOperator("AND");
-		
-		List<Integer> result=lista.codeSmellIds(r, "LOC", "CYCLO");
-		
-		for(int i=0; i!=result.size(); i++) {
-			System.out.println(result.get(i));
-		}
-		
-		excelReader.getMetricValues("LOC");
-		Gui gui = new Gui(excelReader);
-		gui.open();
-
-		RuleSet ruleSet=new RuleSet(excelReader);*/
 		
     }
 	
--- Code-Smell.service/src/main/java/codesmellservice/RuleSet.java
+++ Code-Smell.service/src/main/java/codesmellservice/RuleSet.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * <h1>RuleSet<h1>
+ * A classe RuleSet é uma classe destinada para o armazenamento de todas as regras criadas
+ * pelo utilizador mas também é onde estão implementados os métodos que serão utilizados para detetar
+ * os method id's das regras que o utilizador escolher, que possuem code smells segundo alguns critérios. 
+ * Também é nesta classe que estão implementados métodos que iriam permitir a deteção de defeitos
+ * (ex: DCI, DII, ADCI, ADII) de uma ferramenta ou regra escolhida pelo utilizador. 
+ * 
+ * 
+ * @author Gonçalo Morgado
+ * @since 2020-12-10
+ * 
+ */
 public class RuleSet {
 
 	private List<Rule> lista;
@@ -11,30 +24,96 @@ public class RuleSet {
 	private HashMap<String, Integer> map;
 	private List<String> resultadosBool;
 
+	/**
+	 * O construtor da classe RuleSet é inicializado a partir de uma instância da classe ExcelReader para 
+	 * mais tarde, toda a informação dentro do ficheiro de Excel, ser utilizada nos filtragem dos method id's
+	 * e dos defeitos das ferramentas ou regras.
+	 * @param excel A classe RuleSet utiliza esta objecto para filtrar os method id's, ao comparar os valores de uma regra definida
+	 * pelo utilizador, ao valores no ficheiro Excel
+	 * 
+	 * @author Gonçalo Morgado
+	 * @since 2020-12-10
+	 * 
+	 */
 	public RuleSet(ExcelReader excel) {
 		this.excel=excel;
 		lista=new ArrayList<Rule>();
 		resultadosBool = new ArrayList<String>();
 		this.map = new HashMap<>(); 
 	}
-
-	public void addRegra1(Rule r) {
-		lista.add(r);
+	
+	/**
+	 * Método para adicionar uma regra à lista de regras
+	 * @param rule Regra dada para adicionar à lista de regras 
+	 * 
+	 * @author Gonçalo Morgado
+	 * @since 2020-12-10
+	 * 
+	 */
+	public void addRegra1(Rule rule) {
+		lista.add(rule);
 	}
 
+	/**
+	 * Método para obter a lista de regras definidas pelo utlizador
+	 * @results é devolvida uma lista de todas as regras utilizadas pelo utilizador 
+	 * 
+	 * @author Gonçalo Morgado
+	 * @since 2020-12-10
+	 * 
+	 */
 	public List<Rule> getRegras() {
 		return lista;
 	}
 	
-
+	/**
+	 * Método para obter uma lista de resultados da análise de codeSmells de cada método declarado no ficheiro excel
+	 * @results é devolvida uma lista strings com os resultados da análise
+	 * 
+	 * @author Hugo Silva
+	 * @since 2020-12-10
+	 * 
+	 */
 	public List<String> getResultadosBool(){
 		return this.resultadosBool;
 	}
 	
+	/**
+	 * Método que retorna um HashMap com as siglas dos indicadores de qualidade e os seus respectivos
+	 * valores, de uma determinada regra ou ferramenta.
+	 * 
+	 * @results é devolvido um HashMap com as strings das siglas e os respectivos valores dos indicadores
+	 * de qualidade
+	 * 
+	 * @author Hugo Silva
+	 * @since 2020-12-10
+	 * 
+	 */
 	public HashMap<String, Integer> getMap(){
 		return this.map;
 	}
 	
+	/**
+	 * Neste método é feita a análise de codeSmells de uma determinada regra escolhida pelo utilizador, 
+	 * a partir da comparação de duas listas, onde estas são valores das métricas retiradas do ficheiro 
+	 * excel.
+	 * <p>
+	 * Esta comparação é efetuada com as métricas definidas numa determinada regra criada pelo utilizador,
+	 * onde se podem apresentar vários critérios de comparação, como por exemplo: Se LOC é maior do que 50
+	 * ou CYCLO é menor do que 30, então o método tem um codeSmell.
+	 * <p>
+	 * @param aux String dos operadores relacionais utilizados na regra para a comparação de valores
+	 * @param r Regra escolhida pelo utilizador para sua análise
+	 * @param firstColumn Valores de uma coluna correspondente a uma métrica do ficheiro Excel
+	 * @param secondColumn Valores de uma coluna correspondente a uma métrica do ficheiro Excel
+	 * @results É devolvida uma lista de strings com os resultados (i.e. se tem codeSmell retorna "true" 
+	 * e se não tem codeSmell retorna "false") da comparação das thresholds da regra escolhida pelo utilizador,
+	 * com os valores das colunas das métricas dadas como parâmetro, do ficheiro excel.
+	 * 
+	 * @author Gonçalo Morgado
+	 * @since 2020-12-10
+	 * 
+	 */
 	public List<String> stringValues(String aux, Rule r, List<String> firstColumn, List<String> secondColumn) {
 		List<String> resultados=new ArrayList<String>();
 		System.out.println(r.getLogicalOperator());
@@ -384,8 +463,22 @@ public class RuleSet {
 		return resultados;
 	}
 	
-
-
+	/**
+    * O método tem como função calcular o número de DCI's, DII's, ADCI's e ADII's da regra do utilizador ou da
+    * ferramenta PMD e iPlasma.
+    * O cálculo é feito através da comparação da lista dos resultados dado como parâmetro com a coluna do tipo de 
+    * codeSmell da regra caso não seja PMD ou iPlasma. No caso do PMD ou do iPlasma compara-se a coluna da 
+    * ferramenta PMD / iPlasma com a coluna do is_long_method.
+    * @param tool String da ferramenta / do tipo de codeSmell utilizado na regra definida pelo utilizador
+    * @param resultados Lista de resultados provenientes da comparação das thresholds da regra escolhida pelo               
+    * utilizador, com os valores das colunas das métricas dadas como parâmetro, do ficheiro excel.
+    * @return É devolvido um HashMap<K,V> em que as key's referem-se aos DCI, DII, ADCI e ADII e os Values
+    * referem-se à contagem de cada um deles 
+    * 
+    * @author Hugo Silva
+    * @since 2020-12-10
+    * 
+    */
 	public HashMap<String,Integer> quality_indicators(String tool, List<String> resultados){
 
 		this.map = new HashMap<String,Integer>();
@@ -435,10 +528,21 @@ public class RuleSet {
         return this.map;
     }
     
-	
+	/**
+	 * Este método utiliza dois métodos(stringValues e methodIDS) para obter resultados (se uma determinado
+	 * método tem codeSmell ou não) numa lista de strings e posteriormente filtrar estes resultados para apresentar
+	 * uma lista de inteiros que possui os method Id's que têm codeSmells.
+	 * 
+	 * @param r Regra escolhida pelo utilizador para análise
+	 * @param xMetrica String de uma métrica de um determinado codeSmell (neste caso: LOC ou ATFD)
+	 * @param yMetrica String de uma métrica de um determinado codeSmell (neste caso: CYCLO ou LAA)
+	 * @results devolve uma lista de inteiros dos method id's que têm codeSmells
+	 * 
+	 * @author Gonçalo Morgado
+	 * @since 2020-12-10
+	 * 
+	 */
 	public List<Integer> codeSmellIds(Rule r, String xMetrica, String yMetrica) {
-
-		System.out.println("Tou a entrar na funcao");
 
         List<Integer> valores=new ArrayList<Integer>();
 
@@ -448,18 +552,25 @@ public class RuleSet {
         String s=r.getmetricaXOperator();
         String s2=r.getmetricaYOperator();
         String aux=s+s2;
-        //System.out.println(aux);
-        
 
         this.resultadosBool=stringValues(aux, r, firstColumn, secondColumn);
         valores=methodIDS(this.resultadosBool);
 
-//        for(int i = 0; i != valores.size(); i++){
-//        	System.out.println(valores.get(i));
-//        }
         return valores;
     }
 
+	/**
+	 * Neste método é feita a filtragem de uma lista que contém os resultados dos métodos 
+	 * que sofreram análise anterior, para depois ser apresentado uma lista de inteiros que 
+	 * irá devolver apenas os method ID's que têm codeSmells.
+	 * 
+	 * @param values lista de strings que tem resultados, true ou false, se um determinado método possui codeSmells
+	 * @results devolve uma lista de inteiros dos method id's que têm codeSmells
+	 * 
+	 * @author Gonçalo Morgado
+	 * @since 2020-12-10
+	 * 
+	 */
     public List<Integer> methodIDS(List<String> values)  {
         List<Integer> methodIds=new ArrayList<Integer>();
         for(int i=0; i!=values.size(); i++)  {
@@ -470,8 +581,19 @@ public class RuleSet {
         return methodIds;
     }
     
-    public List<Integer> excelTools(String s) {
-    	List<String> tool = excel.getColumnValues(s);
+    /**
+	 * Neste método, é feita a filtragem dos method ID's que têm codeSmells, apenas das ferramentas (
+	 * iPlasma ou PMD) que estão descritas no Excel.
+	 * 
+	 * @param ferramenta String de uma determinada ferramenta que utilizador escolheu para análise 
+	 * @results devolve uma lista de inteiros dos method id's que têm codeSmells
+	 * 
+	 * @author Gonçalo Morgado
+	 * @since 2020-12-10
+	 * 
+	 */
+    public List<Integer> excelTools(String ferramenta) {
+    	List<String> tool = excel.getColumnValues(ferramenta);
         List<String> methodId= excel.getColumnValues("MethodID");
         List<Integer> results=new ArrayList<Integer>();
         for(int i=0; i!=methodId.size(); i++) {
